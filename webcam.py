77dcda38038a80000d7aa8b5efb85168d7af15a8
Zeki Kam <155576230+ZekiKam@users.noreply.github.com>
2024-08-08 16:09:04 +0100

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,13 +1,114 @@
+# from sbot import Robot
+# import math
+# robot = Robot()
+# # Name the motors to make it easier
+# LeftMotor = robot.motor_board.motors[0]
+# RightMotor = robot.motor_board.motors[1]
+# # Helper function to set motor speeds
+# def set_motors(left, right):
+#     LeftMotor.power = left
+#     RightMotor.power = right
+# # This function checks visible maker list for a specific target
+# def find_marker(markers, id_number):
+#     for marker in markers:
+#         if marker.id == id_number:
+#             return marker
+#     return None
+
+# def loop(target_id):
+#     while True:
+#         set_motors(0, 0) #Stop the robot before taking a photo, avoids motion blur
+#         robot.sleep(0.2) #Time for robot to actually stop
+#         markers = robot.camera.see()
+#         target_marker = find_marker(markers, target_id)
+#         if target_marker != None:
+#             angle_to_marker = math.degrees(target_marker.position.horizontal_angle)
+#             if angle_to_marker > 10: #REPLACE X WITH SUITABLE VALUE
+#                 set_motors(0.05,-0.05)
+#                 robot.sleep(0.25)
+#                 set_motors(0,0)
+#             #ADD CODE HERE TO TURN TOWARD MARKER
+#             elif angle_to_marker < -10: #REPLACE X WITH SUITABLE VALUE
+#                 set_motors(-0.05,0.05)
+#                 robot.sleep(0.25)
+#                 set_motors(0,0)
+#             #ADD CODE HERE TO TURN TOWARD MARKER
+#             elif target_marker.position.distance > 750: #REPLACE Y WITH SUITABLE DISTANCE
+#                 set_motors(0.2,0.2)
+#                 robot.sleep(0.5)
+#                 set_motors(0,0)
+#             elif target_marker.position.distance < 750: #REPLACE Y WITH SUITABLE DISTANCE
+#                 set_motors(-0.15,0.15)
+#                 robot.sleep(0.5)
+#                 set_motors(0,0)
+
+
+
+
+
+# set_motors(0.2, 0.2)
+# robot.sleep(1)
+# set_motors(0,0)
+# target_id = 1
+
+
+# id = [1,3,5]
+# distance = [1000, 1200]
+
+
+            
+            
+# #ADD CODE HERE TO GO FORWARD
+# #ADD CODE HERE TO FIND NEXT MARKER AND COMPLETE HALF A LAP
+# from sbot import Robot
+# import math
+# robot = Robot()
+# # Name the motors to make it easier
+# LeftMotor = robot.motor_board.motors[0]
+# RightMotor = robot.motor_board.motors[1]
+# # Helper function to set motor speeds
+# def set_motors(left, right):
+#     LeftMotor.power = left
+#     RightMotor.power = right
+# # This function checks visible maker list for a specific target
+# def find_marker(markers, id_number):
+#     for marker in markers:
+#         if marker.id == id_number:
+#             return marker
+#     return None
+# set_motors(0.2, 0.2)
+# robot.sleep(1)
+# set_motors(0,0)
+# target_id = 1
+# distance = [1000,1200]
+# target_marker = find_marker(markers, target_id)
+# def stayonline():
+#     if target_marker != None:
+#             angle_to_marker = math.degrees(target_marker.position.horizontal_angle)
+#             if angle_to_marker > 10: #REPLACE X WITH SUITABLE VALUE
+#                 set_motors(0.05,-0.05)
+#                 robot.sleep(0.25)
+#                 set_motors(0,0)
+#             #ADD CODE HERE TO TURN TOWARD MARKER
+#             elif angle_to_marker < -10: #REPLACE X WITH SUITABLE VALUE
+#                 set_motors(-0.05,0.05)
+#                 robot.sleep(0.25)
+#                 set_motors(0,0)
+
+
+#arduino pin A0 is left sensor
+#arduino A1 is right sensor
+
 from sbot import Robot
 import math
 robot = Robot()
-
+offset = 1.05
 # Name the motors to make it easier
 LeftMotor = robot.motor_board.motors[0]
 RightMotor = robot.motor_board.motors[1]
 # Helper function to set motor speeds
 def set_motors(left, right):
-    LeftMotor.power = left
+    LeftMotor.power = left * offset
     RightMotor.power = right
 # This function checks visible maker list for a specific target
 def find_marker(markers, id_number):
@@ -36,6 +137,14 @@ for target_id in [1,3,5,6]:
         robot.sleep(0.2) #Time for robot to actually stop
         markers = robot.camera.see()
         target_marker = find_marker(markers, target_id)
+
+        while target_marker == None:
+            set_motors(-0.1, 0.05)
+            robot.sleep(0.3)
+            if find_marker(markers, target_id) != None:
+                break
+
+        
         if target_marker != None:
             angle_to_marker = math.degrees(target_marker.position.horizontal_angle)
             stayonline()
@@ -50,7 +159,7 @@ for target_id in [1,3,5,6]:
                 elif target_marker.position.distance < 750: #REPLACE Y WITH SUITABLE DISTANCE
                     print("close")
                     set_motors(-0.15,0.15)
-                    robot.sleep(0.5)
+                    robot.sleep(0.75)
                     set_motors(0,0)
                     break
                 
@@ -86,5 +195,4 @@ for target_id in [1,3,5,6]:
                     robot.sleep(0.5)
                     set_motors(0,0)
                     break
-                
                 
