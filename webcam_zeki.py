9b82a7c7bb86e62c1ae38b569cee475ad2238ccb
Zeki Kam <155576230+ZekiKam@users.noreply.github.com>
2024-08-08 12:08:34 +0100

    version 2

--- webcam_zeki.py
+++ webcam_zeki.py
@@ -15,35 +15,39 @@ def find_marker(markers, id_number):
             return marker
     return None
 
-set_motors(0.2, 0.2)
-robot.sleep(1)
-set_motors(0,0)
-target_id = 1
-distance = [1000,1200]
-for target_id in [1,3,5]:
+
+marker_ids = [1, 3, 5]
+target_distances = [750, 1000, 1200]  
+
+
+# Main loop function
+def loop(target_id, target_distance):
     while True:
-        set_motors(0, 0) #Stop the robot before taking a photo, avoids motion blur
-        robot.sleep(0.2) #Time for robot to actually stop
+        set_motors(0, 0)  
+        robot.sleep(0.2)  
         markers = robot.camera.see()
         target_marker = find_marker(markers, target_id)
+
         if target_marker != None:
             angle_to_marker = math.degrees(target_marker.position.horizontal_angle)
-            if angle_to_marker > 10: #REPLACE X WITH SUITABLE VALUE
-                set_motors(0.05,-0.05)
+            if angle_to_marker > 10:  
+                set_motors(0.05, -0.05)  # Turn right
                 robot.sleep(0.25)
-                set_motors(0,0)
-            #ADD CODE HERE TO TURN TOWARD MARKER
-            elif angle_to_marker < -10: #REPLACE X WITH SUITABLE VALUE
-                set_motors(-0.05,0.05)
+                set_motors(0, 0)
+            elif angle_to_marker < -10:  
+                set_motors(-0.05, 0.05)  # Turn left
                 robot.sleep(0.25)
-                set_motors(0,0)
-            #ADD CODE HERE TO TURN TOWARD MARKER
-            elif target_marker.position.distance > 750: #REPLACE Y WITH SUITABLE DISTANCE
-                set_motors(0.2,0.2)
+                set_motors(0, 0)
+            elif target_marker.position.distance > target_distance + 100:  
+                set_motors(0.2, 0.2)
                 robot.sleep(0.5)
-                set_motors(0,0)
-            elif target_marker.position.distance < 750: #REPLACE Y WITH SUITABLE DISTANCE
-                set_motors(-0.15,0.15)
+                set_motors(0, 0)
+            elif target_marker.position.distance < target_distance - 100:  
+                set_motors(-0.15, -0.15)
                 robot.sleep(0.5)
-                set_motors(0,0)
-            elif target_marker.position.distance < distance[0]
+                set_motors(0, 0)
+            else:
+                break  
+
+for i in range(len(marker_ids)):
+    loop(marker_ids[i], target_distances[i])
