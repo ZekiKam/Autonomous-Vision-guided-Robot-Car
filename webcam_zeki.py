b71504493382922c3f9413c883898c39cfefe976
Zeki Kam <155576230+ZekiKam@users.noreply.github.com>
2024-08-09 09:43:42 +0100

    Update webcam_zeki.py

--- webcam_zeki.py
+++ webcam_zeki.py
@@ -1,9 +1,7 @@
 from sbot import Robot
 import math
-import time
 robot = Robot()
-offset = 1.08
-
+offset = 1.05
 # Name the motors to make it easier
 LeftMotor = robot.motor_board.motors[0]
 RightMotor = robot.motor_board.motors[1]
@@ -12,7 +10,7 @@ RightMotor = robot.motor_board.motors[1]
 def set_motors(left, right):
     LeftMotor.power = left * offset
     RightMotor.power = right
-
+    
 # This function checks visible maker list for a specific target
 def find_marker(markers, id_number):
     for marker in markers:
@@ -20,117 +18,110 @@ def find_marker(markers, id_number):
             return marker
     return None
 
-def stayonline():
-    if angle_to_marker > 10: #turn right when the angle is to large
-        print("far right")
-        set_motors(0.15, -0.15)
+def stayonline(angle_to_marker):
+    if angle_to_marker > 10: #REPLACE X WITH SUITABLE VALUE
+        print("FAR RIGHT")
+        set_motors(0.05,-0.05)
         robot.sleep(0.25)
-        time.sleep(0.5)
         set_motors(0,0)
 
-    
+    #ADD CODE HERE TO TURN TOWARD MARKER
     elif angle_to_marker < -10: #REPLACE X WITH SUITABLE VALUE
         print("FAR LEFT")
         set_motors(-0.05,0.05)
         robot.sleep(0.25)
         set_motors(0,0)
-
-distance = [1000,1200]
-for target_id in [1,3,5,6]:
-
+        
+    else: #on track
+        set_motors(0.2,0.2)
+        robot.sleep(0.5)
+        set_motors(0,0)
+    
+def stayonline67(angle_to_marker):
+    if angle_to_marker > 55:
+        print("FAR RIGHT")
+        set_motors(0.05,-0.05)
+        robot.sleep(0.25)
+        set_motors(0,0)
+        
+    elif angle_to_marker > 35:
+        print("FAR LEFT")
+        set_motors(-0.05,0.05)
+        robot.sleep(0.25)
+        set_motors(0,0)
+    
+    else: #on track
+        set_motors(0.2,0.2)
+        robot.sleep(0.5)
+        set_motors(0,0)
+        
+        
+      
+exit = False
+for target_id in [1,3,5,7]:
     while True:
-        set_motors(0, 0) #Stop the robot before taking a photo, avoids motion blur
-        robot.sleep(0.75) #Time for robot to actually stop
+        set_motors(0.05, 0.05) #Stop the robot before taking a photo, avoids motion blur
+        robot.sleep(0.2) #Time for robot to actually stop
         markers = robot.camera.see()
         target_marker = find_marker(markers, target_id)
-
-        if target_marker == None:
-            print("not detected")
-            #search for any markers nearby
-            set_motors(-0.2, 0.2)
-            robot.sleep(0.3)
-            set_motors(0,0)
-        
-        elif target_marker != None:
-            print(f"detected {target_id}")
+        if target_marker != None:
             angle_to_marker = math.degrees(target_marker.position.horizontal_angle)
-            stayonline()
 
             if target_id ==1:
-
-            #ADD CODE HERE TO TURN TOWARD MARKER
-                if target_marker.position.distance > 780: #REPLACE Y WITH SUITABLE DISTANCE
-
-                    print("far away")
-                    set_motors(0.5,0.5)
-                    robot.sleep(0.5)
-                    set_motors(0,0)
-                    robot.sleep(0.5)
-                elif target_marker.position.distance > 750 and target_marker.position.distance< 780:
-                    print("slower")
-                    set_motors(0.2,0.2)
-                    robot.sleep(0.75)
-                    set_motors(0,0)
+                if target_marker.position.distance > 750: #REPLACE Y WITH SUITABLE DISTANCE
+                    print("far away1")
+                    stayonline(angle_to_marker)
                 elif target_marker.position.distance < 750: #REPLACE Y WITH SUITABLE DISTANCE
-
                     print("close")
                     set_motors(-0.15,0.15)
-                    robot.sleep(0.75)
+                    robot.sleep(0.5)
                     set_motors(0,0)
                     break
                 
             elif target_id == 3:
-                if target_marker.position.distance > 1200: #REPLACE Y WITH SUITABLE DISTANCE
-
-                    set_motors(0.5,0.5)
-                    robot.sleep(0.5)
-                    set_motors(0,0)
-                    robot.sleep(0.5)
-                elif target_marker.position.distance > 1000 and target_marker.position.distance< 1200:
-                    print("slower")
-                    set_motors(0.2,0.2)
-                    robot.sleep(0.75)
-                    set_motors(0,0)
+                if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                    print("far away")   
+                    stayonline(angle_to_marker)
                 elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
-
                     set_motors(0.1,-0.1)
                     robot.sleep(0.5)
                     set_motors(0,0)
                     break
                 
             elif target_id == 5:
-                if target_marker.position.distance > 900: #REPLACE Y WITH SUITABLE DISTANCE, rememeber touse reasonable range!
-                    set_motors(0.5,0.5)
-                    robot.sleep(0.75)
-                    set_motors(0,0)
-        
-                elif target_marker.position.distance > 770 and target_marker.position.distance< 900:
-                    print("slower")
-                    set_motors(0.2,0.2)
-                    robot.sleep(0.75)
-                    set_motors(0,0)
-                elif target_marker.position.distance < 770: #REPLACE Y WITH SUITABLE DISTANCE
-
-                    set_motors(-0.4,0.4)
+                if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                    stayonline(angle_to_marker)
+                elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                    set_motors(-0.1,0.1)
                     robot.sleep(0.8)
                     set_motors(0,0)
+                    exit = True
                     break
             
-            elif target_id == 6:
-
-                if target_marker.position.distance > 800: #REPLACE Y WITH SUITABLE DISTANCE
-
-                    set_motors(0.2,0.2)
-                    robot.sleep(0.5)
+            elif target_id == 7:
+                if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                    stayonline67(angle_to_marker)
+                elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                    set_motors(-0.1,0.1)
+                    robot.sleep(0.8)
                     set_motors(0,0)
-                elif target_marker.position.distance > 780 and target_marker.position.distance< 800:
-                    print("slower")
-                    set_motors(0.2,0.2)
-                    robot.sleep(0.75)
-                    set_motors(0,0)
-                elif target_marker.position.distance < 780: #REPLACE Y WITH SUITABLE DISTANCE
-
-                    set_motors(-0.15,0.15)
-                    robot.sleep(0.5)
-                    set_motors(0,0)
+                    exit = True
                     break
+                
+    if exit:
+        break
+
+print("Loop finished")
+ 
+#45 degree turn handled as 90
+set_motors(-0.4,0.4)
+robot.sleep(1)         
+ultra_distance = robot.arduino.ultrasound_measure(2,3)
+while ultra_distance > 500:
+    ultra_distance = robot.arduino.ultrasound_measure(2,3)
+    print("Ultrasound:",ultra_distance)
+    set_motors(0.4,0.4)
+    robot.sleep(0.5)
+    set_motors(0,0)
+    
+set_motors(0,0)
