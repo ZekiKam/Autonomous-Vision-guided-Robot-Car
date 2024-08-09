0aff4a2505f73ec0f63d03225106bfaff2141dd2
Zeki Kam <155576230+ZekiKam@users.noreply.github.com>
2024-08-09 10:46:16 +0100

    version 10

--- webcam_zeki.py
+++ webcam_zeki.py
@@ -11,12 +11,14 @@ def set_motors(left, right):
     LeftMotor.power = left * offset
     RightMotor.power = right
     
+    
 # This function checks visible maker list for a specific target
 def find_marker(markers, id_number):
     for marker in markers:
         if marker.id == id_number:
             return marker
     return None
+
 
 def stayonline(angle_to_marker):
     if angle_to_marker > 10: #REPLACE X WITH SUITABLE VALUE
@@ -37,16 +39,19 @@ def stayonline(angle_to_marker):
         robot.sleep(0.5)
         set_motors(0,0)
     
+    
 def stayonline67(angle_to_marker):
-    if angle_to_marker > 55:
+    if angle_to_marker > -35:
+        print("Angle:",angle_to_marker)
         print("FAR RIGHT")
-        set_motors(0.05,-0.05)
+        set_motors(-0.05,0.05)
         robot.sleep(0.25)
         set_motors(0,0)
         
-    elif angle_to_marker > 35:
+    elif angle_to_marker < -55:
+        print("Angle:",angle_to_marker)
         print("FAR LEFT")
-        set_motors(-0.05,0.05)
+        set_motors(0.05,-0.05)
         robot.sleep(0.25)
         set_motors(0,0)
     
@@ -56,72 +61,77 @@ def stayonline67(angle_to_marker):
         set_motors(0,0)
         
         
-      
-exit = False
-for target_id in [1,3,5,7]:
-    while True:
-        set_motors(0.05, 0.05) #Stop the robot before taking a photo, avoids motion blur
-        robot.sleep(0.2) #Time for robot to actually stop
-        markers = robot.camera.see()
-        target_marker = find_marker(markers, target_id)
-        if target_marker != None:
-            angle_to_marker = math.degrees(target_marker.position.horizontal_angle)
+while True:
+    exit = False
+    for target_id in [1,3,5,7]:
+        while True:
+            set_motors(0,0) #Stop the robot before taking a photo, avoids motion blur
+            robot.sleep(0.2) #Time for robot to actually stop
+            markers = robot.camera.see()
+            target_marker = find_marker(markers, target_id)
+            print(f"DETECTED:{target_marker}")
+            if target_marker != None:
+                angle_to_marker = math.degrees(target_marker.position.horizontal_angle)
 
-            if target_id ==1:
-                if target_marker.position.distance > 750: #REPLACE Y WITH SUITABLE DISTANCE
-                    print("far away1")
-                    stayonline(angle_to_marker)
-                elif target_marker.position.distance < 750: #REPLACE Y WITH SUITABLE DISTANCE
-                    print("close")
-                    set_motors(-0.15,0.15)
-                    robot.sleep(0.5)
-                    set_motors(0,0)
-                    break
+                if target_id ==1:
+                    if target_marker.position.distance > 750: #REPLACE Y WITH SUITABLE DISTANCE
+                        print("far away1")
+                        stayonline(angle_to_marker)
+                    elif target_marker.position.distance < 750: #REPLACE Y WITH SUITABLE DISTANCE
+                        print("close")
+                        set_motors(-0.15,0.15)
+                        robot.sleep(0.5)
+                        set_motors(0,0)
+                        break
+                    
+                elif target_id == 3:
+                    if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                        print("far away")   
+                        stayonline(angle_to_marker)
+                    elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                        set_motors(0.1,-0.1)
+                        robot.sleep(0.5)
+                        set_motors(0,0)
+                        break
+                    
+                elif target_id == 5:
+                    if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                        stayonline(angle_to_marker)
+                    elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                        set_motors(-0.1,0.1)
+                        robot.sleep(0.8)
+                        set_motors(0,0)
+                        exit = True
+                        break
                 
-            elif target_id == 3:
-                if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
-                    print("far away")   
-                    stayonline(angle_to_marker)
-                elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
-                    set_motors(0.1,-0.1)
-                    robot.sleep(0.5)
-                    set_motors(0,0)
-                    break
-                
-            elif target_id == 5:
-                if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
-                    stayonline(angle_to_marker)
-                elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
-                    set_motors(-0.1,0.1)
-                    robot.sleep(0.8)
-                    set_motors(0,0)
-                    exit = True
-                    break
+                elif target_id == 7:
+                    if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                        set_motors(0.2,0.2)
+                        robot.sleep(0.8)                        
+                    elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
+                        set_motors(-0.1,0.1)
+                        robot.sleep(0.8)
+                        set_motors(0,0)
+                        exit = True
+                        break
             
-            elif target_id == 7:
-                if target_marker.position.distance > 1000: #REPLACE Y WITH SUITABLE DISTANCE
-                    stayonline67(angle_to_marker)
-                elif target_marker.position.distance < 1000: #REPLACE Y WITH SUITABLE DISTANCE
-                    set_motors(-0.1,0.1)
-                    robot.sleep(0.8)
-                    set_motors(0,0)
-                    exit = True
-                    break
-                
-    if exit:
-        break
+            else:
+                set_motors(0.15,-0.15)
+                robot.sleep(0.2)
+                set_motors(0,0)
+
 
 print("Loop finished")
  
-#45 degree turn handled as 90
-set_motors(-0.4,0.4)
-robot.sleep(1)         
-ultra_distance = robot.arduino.ultrasound_measure(2,3)
-while ultra_distance > 500:
-    ultra_distance = robot.arduino.ultrasound_measure(2,3)
-    print("Ultrasound:",ultra_distance)
-    set_motors(0.4,0.4)
-    robot.sleep(0.5)
-    set_motors(0,0)
+# #45 degree turn handled as 90
+# set_motors(-0.1,0.1)
+# robot.sleep(1)         
+# ultra_distance = robot.arduino.ultrasound_measure(2,3)
+# while ultra_distance > 500:
+#     ultra_distance = robot.arduino.ultrasound_measure(2,3)
+#     print("Ultrasound:",ultra_distance)
+#     set_motors(0.4,0.4)
+#     robot.sleep(0.5)
+#     set_motors(0,0)
     
-set_motors(0,0)
+# set_motors(0,0)
